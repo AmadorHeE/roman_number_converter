f7c157e21a4c7a12f4f82d4d75426de1862f7547
AmadorHeE <amador.e.git@gmail.com>
2024-01-22 05:22:49 +0000

    feat: Implement the command line interface to convert from roman numbers to arabic numbers and the other way around.

---
+++ README.md
@@ -0,0 +1,13 @@
+# Roman number converter
+
+## Documentation
+
+The task is implement a method to convert a arabic number representation (common one) to roman number format and the other way around.
+
+### Test the CLI.
+
+To run the CLI you can use the command ` mvn clean package` to build the project and then run the command `java -jar target/roman-number-converter-1.0-SNAPSHOT.jar ROMAN_TO_ARABIC V` or `java -jar target/roman-number-converter-1.0-SNAPSHOT.jar ARABIC_TO_ROMAN 1`
+
+### Run the test
+
+To execute the test run the command `mvn test`
--- src/main/java/com/eamador/App.java
+++ src/main/java/com/eamador/App.java
@@ -1,13 +1,45 @@
 package com.eamador;
 
-/**
- * Hello world!
- *
- */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+public class App {
+    private static final String ROMAN_TO_ARABIC = "ROMAN_TO_ARABIC";
+    private static final String ARABIC_TO_ROMAN = "ARABIC_TO_ROMAN";
+
+    public static void main( String[] args ) {
+        if (args.length < 2) {
+            System.out.println("Please provide two arguments: operation(ARABIC_TO_ROMAN | ARABIC_TO_ROMAN) type and value to convert.");
+            return;
+        }
+
+        String operation = args[0];
+        String value = args[1];
+        
+        if (operation == null || operation.isEmpty()) {
+            System.out.println("Please provide a valid operation(ARABIC_TO_ROMAN | ROMAN_TO_ARABIC).");
+            return;
+        }
+
+        if (value == null || value.isEmpty()) {
+            System.out.println("Please provide a valid value to convert.");
+            return;
+        }
+
+        if (operation.equals(ROMAN_TO_ARABIC)) {
+            try {
+                RomanNumber romanNumber = new RomanNumber(value);
+                System.out.println(romanNumber.getArabicNumber().getValue());
+            } catch (IllegalArgumentException e) {
+                System.out.println("Erro converting roman number " + e.getMessage());
+            }
+        } else if (operation.equals(ARABIC_TO_ROMAN)) {
+            try {
+                ArabicNumber arabicNumber = new ArabicNumber(Integer.parseInt(value));
+                System.out.println(arabicNumber.getRomanNumber().getValue());
+            } catch (IllegalArgumentException e) {
+                System.out.println("Erro converting arabic number " + e.getMessage());
+            }
+        } else {
+            System.out.println("Please provide a valid operation(ARABIC_TO_ROMAN | ARABIC_TO_ROMAN).");
+        }
+        
     }
 }
